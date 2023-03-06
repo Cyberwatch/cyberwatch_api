acb5414cf9a231921ceaf515a1f21826c7ded08b
Daniel Deutsch <daniel@cyberwatch.fr>
2023-03-06 13:56:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,11 @@ from setuptools import setup, find_packages
 setup(name='cyberwatch_api',
       version='0.1.0',
       description='Python Api client for the Cyberwatch software',
+      long_description=open('README.md').read().strip(),
+      long_description_content_type="text/markdown"
       author='CyberWatch SAS',
       packages=find_packages(),
       py_modules=['cyberwatch_api'],
+      license='MIT',
       install_requires=['requests>=2.20.1']
       )
