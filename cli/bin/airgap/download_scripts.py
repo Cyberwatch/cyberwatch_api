4c9ab5ec26707ca5f5e7c895aa6f8c535d2280cb
Ubuntu <a.aitkaci@yahoo.fr>
2023-07-13 09:06:42 +0000

    cyberwatch-cli tool with the airgap module

--- cli/bin/airgap/download_scripts.py
+++ cli/bin/airgap/download_scripts.py
@@ -4,6 +4,7 @@ from itertools import groupby
 import argparse
 import sys
 import os
+import requests
 
 def help():
     print("Usage : " + str(sys.argv[0]) + " airgap download-scripts [ARGS]")
@@ -33,7 +34,7 @@ def retrieve_scripts(scriptID=""):
     )
     return next(apiResponse).json()
 
-def download_scripts(destination_folder):
+def download_scripts(destination_folder, with_attachment=False):
     script_dir = join(abspath(destination_folder), "scripts")
     upload_dir = join(abspath(destination_folder), "uploads")
     os.makedirs(script_dir, exist_ok=True)
@@ -42,7 +43,7 @@ def download_scripts(destination_folder):
     scripts_metadata = retrieve_scripts()
     print("Downloading scripts..")
     # Downloading every single script
-    saved_scripts = [download_individual_script(script["id"], script_dir) for script in scripts_metadata]
+    saved_scripts = [download_individual_script(script["id"], script_dir, with_attachment) for script in scripts_metadata]
     # Grouping script by OS
     grouped_scripts = groupby(sorted(saved_scripts), lambda x: x[0])
 
@@ -69,7 +70,7 @@ def append_extension(target_os):
     if target_os == "Windows": return ".ps1"
     return ""
 
-def download_individual_script(scriptID, base_dir, with_attachment=False,):
+def download_individual_script(scriptID, base_dir, with_attachment=False):
     script = retrieve_scripts(str(scriptID))
     if script is None or script["type"] is None: return None, None
 
@@ -77,19 +78,23 @@ def download_individual_script(scriptID, base_dir, with_attachment=False,):
     script_dir = join(base_dir + "/" + target_os)
     script_filename = script_name + append_extension(target_os)
 
+    # Save the script in the correct directory
     os.makedirs(dirname(join(script_dir, script_filename)), exist_ok=True)
     with open(join(script_dir, script_filename), "w") as scriptFile:
         scriptFile.write(script["contents"])
 
-    # if script.attachment and with_attachment:
-    #     download_attachment(directory=dirname(script_filename), url=script.attachment)
+    # Download the attachment if it exists
+    if script["attachment"] and with_attachment:
+        attachment = requests.get(script["attachment"], allow_redirects=True, verify=False)
+        with open(join(script_dir, basename(script["attachment"])), "wb") as attachmentFile:
+            attachmentFile.write(attachment.content)
 
     print("\033[A\033[A\nDownloaded script : " + str(script_name) + " " * 40)
     return target_os, script_filename
 
 def manager(arguments):
     parser = argparse.ArgumentParser()
-    parser.add_argument("--no-attachement", action="store_true")
+    parser.add_argument("--no-attachment", action="store_false")
     parser.add_argument("--dest-dir", default="cyberwatch-airgap")
     parser.add_argument("help", nargs="?")
     options = parser.parse_args(arguments)
@@ -97,4 +102,4 @@ def manager(arguments):
     if arguments and arguments[0] == "help":
         help()
     else:
-        download_scripts(options.dest_dir)
+        download_scripts(options.dest_dir, options.no_attachment)
