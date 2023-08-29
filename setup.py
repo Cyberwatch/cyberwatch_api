e792d55c3f351244cb1c6f37164d035bb6761a1a
Ubuntu <a.aitkaci@yahoo.fr>
2023-08-29 12:13:07 +0200

    Fix imports and package resolution, and updated requirements

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='cyberwatch_api',
-      version='0.3.3',
+      version='0.3.5',
       description='Python Api client for the Cyberwatch software',
       long_description=open('README.md').read().strip(),
       long_description_content_type="text/markdown",
       author='CyberWatch SAS',
-      packages=find_packages(where='cli'),
-      package_dir={"": "cli"},
       py_modules=['cyberwatch_api'],
+      packages=find_packages(include=['cli.*']),
       license='MIT',
       install_requires=['requests>=2.20.1'],
       scripts=['cli/cyberwatch-cli']
