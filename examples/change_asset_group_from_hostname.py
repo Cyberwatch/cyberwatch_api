fe02ee7afdefb7affcdff75f1a3bb1dbe398a4c2
SamCbw <156076333+SamCbw@users.noreply.github.com>
2024-01-09 11:41:39 +0100

    Add new example
    This script can change asset's group, based on their hostname. Hostnames are read from a file, where there is one hostname per line.

---
+++ examples/change_asset_group_from_hostname.py
@@ -0,0 +1,45 @@
+# This script will replace asset's groups by another one
+from cyberwatch_api import Cyberwatch_Pyhelper
+import json
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+
+# Suppress only the single warning from urllib3 needed.
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+####################################################
+########## Définition des variables ################
+####################################################
+ 
+idgrp = 10 # Id du groupe qui écrasera ceux existant sur les actifs 
+ 
+####################################################
+ 
+def retrieve_assets():
+    """retrieve all assets for a cyberwatch node"""
+    assets = {}
+    apiResponse = Cyberwatch_Pyhelper().request(
+        method="GET",
+        endpoint="/api/v3/vulnerabilities/servers",
+        verify_ssl=False
+    )
+    print("Retrieving assets : 0")
+    for page in apiResponse:
+        for asset in page.json():
+            assets[asset['hostname']] = asset['id']
+        print("\033[A\033[A\nRetrieving assets : " + str(len(assets)))
+    return assets
+ 
+ALL_ASSETS = retrieve_assets()
+ 
+with open('liste_machines', 'r') as f:
+    hostnames = [line.strip() for line in f]
+ 
+for hostname in hostnames:
+    output = Cyberwatch_Pyhelper().request(
+        method="PUT",
+        endpoint="/api/v3/vulnerabilities/servers/" + str(ALL_ASSETS.get(hostname)),
+        body_params={'groups' : [idgrp]},
+        verify_ssl=False
+    )
+    next(output)
