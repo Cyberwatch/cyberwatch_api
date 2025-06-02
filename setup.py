e6f7573ac7288fb4cfadb47f4c03a39bbba0a806
Daniel D. <53611126+fenrisl@users.noreply.github.com>
2025-06-02 07:29:44 +0000

    Added chardet dependency

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup, find_packages
 
-setup(name='cyberwatch_api',
-      version='0.3.7',
-      description='Python Api client for the Cyberwatch software',
-      long_description=open('README.md').read().strip(),
-      long_description_content_type="text/markdown",
-      author='CyberWatch SAS',
-      py_modules=['cyberwatch_api'],
-      packages=find_packages(include=['cli.*']),
-      license='MIT',
-      install_requires=['requests>=2.20.1'],
-      scripts=['cli/cyberwatch-cli']
-      )
+setup(
+    name='cyberwatch_api',
+    version='0.3.8',
+    description='Python Api client for the Cyberwatch software',
+    long_description=open('README.md').read().strip(),
+    long_description_content_type="text/markdown",
+    author='CyberWatch SAS',
+    py_modules=['cyberwatch_api'],
+    packages=find_packages(include=['cli.*']),
+    license='MIT',
+    install_requires=[
+        'requests>=2.20.1',
+        'chardet'
+    ],
+    scripts=['cli/cyberwatch-cli']
+)
